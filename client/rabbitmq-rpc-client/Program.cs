11ddcce61a51ce65d5c221ac9e4aaf2d88cd106f
ABrandaoL <alexandre.brandao.lustosa@gmail.com>
2020-07-21 22:21:37 -0300

    Ajustes

---
+++ client/rabbitmq-rpc-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rabbitmq_rpc_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
